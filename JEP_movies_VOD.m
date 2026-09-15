f4038064f3b7d4dcef95c0af1d9dfc14fd44871d
Peralta, José <peralta.je@gmail.com>
2026-09-15 10:00:59 -0300

    Add files via upload

---
+++ JEP_movies_VOD.m
@@ -0,0 +1,152 @@
+#EXTM3U
+
+#EXTINF:-1 type="movie" tvg-id="space-mutiny-1988" tvg-name="Space Mutiny" tvg-logo="https://archive.org/services/img/space-mutiny-1988" group-title="VOD | Películas | 1980-1989 | Ciencia ficción / Acción",Space Mutiny (1988)
+https://archive.org/download/space-mutiny-1988/Space%20Mutiny%20%281988%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="miami-connection-1987" tvg-name="Miami Connection" tvg-logo="https://archive.org/services/img/miami-connection-1987" group-title="VOD | Películas | 1980-1989 | Acción / Artes Marciales",Miami Connection (1987)
+https://archive.org/download/miami-connection-1987/Miami%20Connection%20%281987%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="escape-from-sobibor-1987" tvg-name="Escape from Sobibor" tvg-logo="https://archive.org/services/img/escape-from-sobibor-1987" group-title="VOD | Películas | 1980-1989 | Drama / Historia",Escape from Sobibor (1987)
+https://archive.org/download/escape-from-sobibor-1987/Escape%20From%20Sobibor%20%281987%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="a-christmas-without-snow-1980" tvg-name="A Christmas Without Snow" tvg-logo="https://archive.org/services/img/a-christmas-without-snow-1980" group-title="VOD | Películas | 1980-1989 | Drama / Música",A Christmas Without Snow (1980)
+https://archive.org/download/a-christmas-without-snow-1980/A%20Christmas%20Without%20Snow%20%281980%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="metalstorm-the-destruction-of-jared-syn-1983" tvg-name="Metalstorm The Destruction Of Jared Syn 1983" tvg-logo="https://archive.org/services/img/metalstorm-the-destruction-of-jared-syn-1983" group-title="VOD | Películas | 1980-1989 | Ciencia ficción",Metalstorm The Destruction Of Jared Syn 1983 (1983)
+https://archive.org/download/metalstorm-the-destruction-of-jared-syn-1983/Metalstorm%20-%20The%20Destruction%20of%20Jared-Syn%201983.mp4
+
+#EXTINF:-1 type="movie" tvg-id="microcosmos-1996" tvg-name="Microcosmos" tvg-logo="https://archive.org/download/microcosmos-1996/e80e5e78d8faf868d1f76a0a9b8c233b57c250cb7000fdb9f2161d854df9dce5.jpg" group-title="VOD | Películas | 1990-1999 | Documental / Naturaleza",Microcosmos (1996)
+https://archive.org/download/microcosmos-1996/Documental%20-%20Microcosmos%20%281996%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="project-metalbeast-1995" tvg-name="PROJECT METALBEAST 1995" tvg-logo="https://archive.org/services/img/project-metalbeast-1995" group-title="VOD | Películas | 1990-1999 | Terror / Ciencia ficción",PROJECT METALBEAST 1995 (1995)
+https://archive.org/download/project-metalbeast-1995/PROJECT_METALBEAST%201995.mp4
+
+#EXTINF:-1 type="movie" tvg-id="wax-or-the-discovery-of-television-among-the-bees-1991" tvg-name="Wax or the Discovery of Television Among the Bees" tvg-logo="https://archive.org/services/img/wax-or-the-discovery-of-television-among-the-bees-1991" group-title="VOD | Películas | 1990-1999 | Ciencia ficción / Culto",Wax or the Discovery of Television Among the Bees (1991)
+https://archive.org/download/wax-or-the-discovery-of-television-among-the-bees-1991/Wax%20or%20the%20Discovery%20of%20Television%20Among%20the%20Bees%20%281991%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="BigBuckBunny_328" tvg-name="Big Buck Bunny" tvg-logo="https://archive.org/services/img/BigBuckBunny_328" group-title="VOD | Películas | 2000-2009 | Animación / Comedia",Big Buck Bunny (2008)
+https://archive.org/download/BigBuckBunny_328/BigBuckBunny_512kb.mp4
+
+#EXTINF:-1 type="movie" tvg-id="ElephantsDream" tvg-name="Elephants Dream" tvg-logo="https://archive.org/download/ElephantsDream/ED-CM-St-16bit.png" group-title="VOD | Películas | 2000-2009 | Animación / Ciencia ficción",Elephants Dream (2006)
+https://archive.org/download/ElephantsDream/ed_1024.mp4
+
+#EXTINF:-1 type="movie" tvg-id="Sita_Sings_the_Blues" tvg-name="Sita Sings the Blues" tvg-logo="https://archive.org/download/Sita_Sings_the_Blues/SSTB_MASTER_LoRo_01.R_spectrogram.png" group-title="VOD | Películas | 2000-2009 | Animación / Musical",Sita Sings the Blues (2008)
+https://archive.org/download/Sita_Sings_the_Blues/Sita_Sings_the_Blues_1080p.mp4
+
+#EXTINF:-1 type="movie" tvg-id="BBS.The.Documentary" tvg-name="BBS: The Documentary" tvg-logo="https://archive.org/services/img/BBS.The.Documentary" group-title="VOD | Películas | 2000-2009 | Documental / Tecnología",BBS: The Documentary (2005)
+https://archive.org/download/BBS.The.Documentary/BBS.The.Documentary.ep3_512kb.mp4
+
+#EXTINF:-1 type="movie" tvg-id="Sintel" tvg-name="Sintel" tvg-logo="https://archive.org/download/Sintel/Poster.jpg" group-title="VOD | Películas | 2010-2019 | Animación / Fantasía",Sintel (2010)
+https://archive.org/download/Sintel/sintel-2048-surround.mp4
+
+#EXTINF:-1 type="movie" tvg-id="Tears-of-Steel" tvg-name="Tears of Steel" tvg-logo="https://archive.org/download/Tears-of-Steel/tos-poster.jpg" group-title="VOD | Películas | 2010-2019 | Animación / Ciencia ficción",Tears of Steel (2012)
+https://archive.org/download/Tears-of-Steel/tears_of_steel_720p.mp4
+
+#EXTINF:-1 type="movie" tvg-id="TheInternetsOwnBoyTheStoryOfAaronSwartz" tvg-name="The Internet's Own Boy: The Story of Aaron Swartz" tvg-logo="https://archive.org/services/img/TheInternetsOwnBoyTheStoryOfAaronSwartz" group-title="VOD | Películas | 2010-2019 | Documental / Biografía",The Internet's Own Boy: The Story of Aaron Swartz (2014)
+https://archive.org/download/TheInternetsOwnBoyTheStoryOfAaronSwartz/TheInternetsOwnBoy_TheStoryofAaronSwartz-HD.mp4
+
+#EXTINF:-1 type="movie" tvg-id="CosmosLaundromatFirstCycle" tvg-name="Cosmos Laundromat: First Cycle" tvg-logo="https://archive.org/services/img/CosmosLaundromatFirstCycle" group-title="VOD | Películas | 2010-2019 | Animación / Ciencia ficción",Cosmos Laundromat: First Cycle (2015)
+https://archive.org/download/CosmosLaundromatFirstCycle/Cosmos%20Laundromat%20-%20First%20Cycle%20%281080p%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="Escape_From_Sobibor.avi" tvg-name="Escape From Sobibor" tvg-logo="https://archive.org/services/img/Escape_From_Sobibor.avi" group-title="VOD | Películas | 1980-1989 | Cine / General",Escape From Sobibor (1987)
+https://archive.org/download/Escape_From_Sobibor.avi/Escape_from_Sobibor.mp4
+
+#EXTINF:-1 type="movie" tvg-id="filmcollectief-00-0013" tvg-name="Het is weer zomer in Zandvoort!" tvg-logo="https://archive.org/services/img/filmcollectief-00-0013" group-title="VOD | Películas | 1980-1989 | Cine / General",Het is weer zomer in Zandvoort! (1985)
+https://archive.org/download/filmcollectief-00-0013/het_is_weer_zomer.mp4
+
+#EXTINF:-1 type="movie" tvg-id="adi-vasfiye_movie" tvg-name="Adı Vasfiye, Turkish Movie" tvg-logo="https://archive.org/services/img/adi-vasfiye_movie" group-title="VOD | Películas | 1980-1989 | Cine / General",Adı Vasfiye, Turkish Movie (1985)
+https://archive.org/download/adi-vasfiye_movie/Ad%C4%B1%20Vasfiye%20-%20T%C3%BCrk%20Filmi%20%28M%C3%BCjde%20Ar%20%26%20Y%C4%B1lmaz%20Zafer%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="HighRisk" tvg-name="High Risk" tvg-logo="https://archive.org/services/img/HighRisk" group-title="VOD | Películas | 1980-1989 | Cine / General",High Risk (1981)
+https://archive.org/download/HighRisk/High_Risk.mp4
+
+#EXTINF:-1 type="movie" tvg-id="cco_virus" tvg-name="Virus (Fukkatsu no hi )" tvg-logo="https://archive.org/services/img/cco_virus" group-title="VOD | Películas | 1980-1989 | Cine / General",Virus (Fukkatsu no hi ) (1980)
+https://archive.org/download/cco_virus/ccoPublicDomainVirus.mp4
+
+#EXTINF:-1 type="movie" tvg-id="fallen.-angels.-1995.-blu-ray.-720p.x-264.-dts-my-silu.eng" tvg-name="Fallen Angels" tvg-logo="https://archive.org/services/img/fallen.-angels.-1995.-blu-ray.-720p.x-264.-dts-my-silu.eng" group-title="VOD | Películas | 1990-1999 | Cine / General",Fallen Angels (1995)
+https://archive.org/download/fallen.-angels.-1995.-blu-ray.-720p.x-264.-dts-my-silu.eng/Fallen.Angels.1995.BluRay.720p.x264.DTS-.mp4
+
+#EXTINF:-1 type="movie" tvg-id="la-belle-noiseuse-1991-1080p-x-265-10bit-tigole" tvg-name="La Belle Noiseuse" tvg-logo="https://archive.org/services/img/la-belle-noiseuse-1991-1080p-x-265-10bit-tigole" group-title="VOD | Películas | 1990-1999 | Cine / General",La Belle Noiseuse (1991)
+https://archive.org/download/la-belle-noiseuse-1991-1080p-x-265-10bit-tigole/La%20belle%20noiseuse%20%281991%29%20%281080p%20x265%2010bit%20Tigole%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="ferid-boughedir-directed-1990-movie" tvg-name="Férid Boughedir Directed 1990 Movie" tvg-logo="https://archive.org/services/img/ferid-boughedir-directed-1990-movie" group-title="VOD | Películas | 1990-1999 | Cine / General",Férid Boughedir Directed 1990 Movie (1990)
+https://archive.org/download/ferid-boughedir-directed-1990-movie/F%C3%A9rid%20Boughedir%20directed%201990%20movie.mp4
+
+#EXTINF:-1 type="movie" tvg-id="manufacturing_consent" tvg-name="Manufacturing Consent: Noam Chomsky and the Media" tvg-logo="https://archive.org/services/img/manufacturing_consent" group-title="VOD | Películas | 1990-1999 | Cine / General",Manufacturing Consent: Noam Chomsky and the Media (1993)
+https://archive.org/download/manufacturing_consent/Noam_Chomsky-Manufacturing_Consent_512kb.mp4
+
+#EXTINF:-1 type="movie" tvg-id="the.-river.-1997.-chinese.-1080p.-blu-ray.-h-264.-aac-vxt" tvg-name="The River" tvg-logo="https://archive.org/services/img/the.-river.-1997.-chinese.-1080p.-blu-ray.-h-264.-aac-vxt" group-title="VOD | Películas | 1990-1999 | Cine / General",The River (1997)
+https://archive.org/download/the.-river.-1997.-chinese.-1080p.-blu-ray.-h-264.-aac-vxt/The.River.1997.CHINESE.1080p.BluRay.H264.AAC-VXT.mp4
+
+#EXTINF:-1 type="movie" tvg-id="walking-with-dinosaurs" tvg-name="Walking with Dinosaurs" tvg-logo="https://archive.org/services/img/walking-with-dinosaurs" group-title="VOD | Películas | 1990-1999 | Cine / General",Walking with Dinosaurs (1999)
+https://archive.org/download/walking-with-dinosaurs/Episode%206%20-%20Death%20of%20a%20Dynasty.mp4
+
+#EXTINF:-1 type="movie" tvg-id="perfectblue1997" tvg-name="Perfect Blue 1997" tvg-logo="https://archive.org/services/img/perfectblue1997" group-title="VOD | Películas | 1990-1999 | Cine / General",Perfect Blue 1997 (1997)
+https://archive.org/download/perfectblue1997/Perfect%20Blue%201997.mp4
+
+#EXTINF:-1 type="movie" tvg-id="StarWreckInThePirkining" tvg-name="Star Wreck: In the Pirkining" tvg-logo="https://archive.org/download/StarWreckInThePirkining/StarWreck.png" group-title="VOD | Películas | 2000-2009 | Cine / General",Star Wreck: In the Pirkining (2005)
+https://archive.org/download/StarWreckInThePirkining/StarWreck_512kb.mp4
+
+#EXTINF:-1 type="movie" tvg-id="The_Art_of_Sitting" tvg-name="The Art of Sitting" tvg-logo="https://archive.org/services/img/The_Art_of_Sitting" group-title="VOD | Películas | 2000-2009 | Cine / General",The Art of Sitting (2005)
+https://archive.org/download/The_Art_of_Sitting/The_Art_of_Sitting_512kb.mp4
+
+#EXTINF:-1 type="movie" tvg-id="visitor.-q.-2001.i-nternal.-dvdrip.-xvi-d-i-ls" tvg-name="Visitor Q" tvg-logo="https://archive.org/services/img/visitor.-q.-2001.i-nternal.-dvdrip.-xvi-d-i-ls" group-title="VOD | Películas | 2000-2009 | Cine / General",Visitor Q (2001)
+https://archive.org/download/visitor.-q.-2001.i-nternal.-dvdrip.-xvi-d-i-ls/Visitor.Q.2001.iNTERNAL.DVDRip.XviD-iLS.mp4
+
+#EXTINF:-1 type="movie" tvg-id="zeitgeist_movie" tvg-name="Zeitgeist - The Movie" tvg-logo="https://archive.org/services/img/zeitgeist_movie" group-title="VOD | Películas | 2000-2009 | Cine / General",Zeitgeist - The Movie (2007)
+https://archive.org/download/zeitgeist_movie/Zeitgeist.mp4
+
+#EXTINF:-1 type="movie" tvg-id="Gamer_Br_EnglishVersion" tvg-name="Gamer Br" tvg-logo="https://archive.org/services/img/Gamer_Br_EnglishVersion" group-title="VOD | Películas | 2000-2009 | Cine / General",Gamer Br (2005)
+https://archive.org/download/Gamer_Br_EnglishVersion/Gamer_Br_EnglishVersion_512kb.mp4
+
+#EXTINF:-1 type="movie" tvg-id="youtube-UxME8pxrKyQ" tvg-name="Youtube: UxME8pxrKyQ - Midori - Shoujo Tsubaki - 地下幻燈劇画 少女椿" tvg-logo="https://archive.org/download/youtube-UxME8pxrKyQ/Midori_-_Shoujo_Tsubaki_-_English_sub-UxME8pxrKyQ.jpg" group-title="VOD | Películas | 2010-2019 | Cine / General",Youtube: UxME8pxrKyQ - Midori - Shoujo Tsubaki - 地下幻燈劇画 少女椿 (2013)
+https://archive.org/download/youtube-UxME8pxrKyQ/Midori_-_Shoujo_Tsubaki_-_English_sub-UxME8pxrKyQ.mp4
+
+#EXTINF:-1 type="movie" tvg-id="youtube-e2RdBobQc-c" tvg-name="God's phone number is 1-240-776-2323 and you can send a text to Him" tvg-logo="https://archive.org/download/youtube-e2RdBobQc-c/God_s_phone_number_is_1-240-776-2323_and_you_can_send_a_text_to_Him.-e2RdBobQc-c.jpg" group-title="VOD | Películas | 2010-2019 | Cine / General",God's phone number is 1-240-776-2323 and you can send a text to Him (2011)
+https://archive.org/download/youtube-e2RdBobQc-c/God_s_phone_number_is_1-240-776-2323_and_you_can_send_a_text_to_Him.-e2RdBobQc-c.mkv
+
+#EXTINF:-1 type="movie" tvg-id="knock-knock_202209" tvg-name="Knock Knock" tvg-logo="https://archive.org/services/img/knock-knock_202209" group-title="VOD | Películas | 2010-2019 | Cine / General",Knock Knock (2015)
+https://archive.org/download/knock-knock_202209/Knock%20Knock.mp4
+
+#EXTINF:-1 type="movie" tvg-id="desire_202008" tvg-name="Desiree" tvg-logo="https://archive.org/services/img/desire_202008" group-title="VOD | Películas | 2010-2019 | Cine / General",Desiree (2014)
+https://archive.org/download/desire_202008/Desiree.mp4
+
+#EXTINF:-1 type="movie" tvg-id="youtube-ajaa-XKbgyo" tvg-name="MYSTERY BANG" tvg-logo="https://archive.org/download/youtube-ajaa-XKbgyo/ajaa-XKbgyo.webp" group-title="VOD | Películas | 2020-2029 | Cine / General",MYSTERY BANG (2023)
+https://archive.org/download/youtube-ajaa-XKbgyo/ajaa-XKbgyo.mp4
+
+#EXTINF:-1 type="movie" tvg-id="hellhole.-1985.1080p.-blu-ray.-h-264.-aac-rarbg" tvg-name="Hellhole" tvg-logo="https://archive.org/services/img/hellhole.-1985.1080p.-blu-ray.-h-264.-aac-rarbg" group-title="VOD | Películas | 1980-1989 | Terror",Hellhole (1985)
+https://archive.org/download/hellhole.-1985.1080p.-blu-ray.-h-264.-aac-rarbg/Hellhole.1985.1080p.BluRay.H264.AAC-RARBG.mp4
+
+#EXTINF:-1 type="movie" tvg-id="turner_video_392436" tvg-name="The Slumber Party Massacre" tvg-logo="https://archive.org/services/img/turner_video_392436" group-title="VOD | Películas | 1980-1989 | Terror",The Slumber Party Massacre (1982)
+https://archive.org/download/turner_video_392436/392436.mp4
+
+#EXTINF:-1 type="movie" tvg-id="PhimMaCy1981TheEvilDead1981FullEpisodeVietsub" tvg-name="The Evil Dead" tvg-logo="https://archive.org/services/img/PhimMaCy1981TheEvilDead1981FullEpisodeVietsub" group-title="VOD | Películas | 1980-1989 | Terror",The Evil Dead (1983)
+https://archive.org/download/PhimMaCy1981TheEvilDead1981FullEpisodeVietsub/Phim%20ma%20c%C3%A2y%201981%20-%20%5BThe%20Evil%20Dead%201981%5D%20-%20Full%20episode%20Vietsub.mp4
+
+#EXTINF:-1 type="movie" tvg-id="BloodyWednesday" tvg-name="Bloody Wednesday" tvg-logo="https://archive.org/services/img/BloodyWednesday" group-title="VOD | Películas | 1980-1989 | Terror",Bloody Wednesday (1987)
+https://archive.org/download/BloodyWednesday/BloodyWednesday.mp4
+
+#EXTINF:-1 type="movie" tvg-id="thehorrorofitallreel2" tvg-name="The Horror of it all" tvg-logo="https://archive.org/services/img/thehorrorofitallreel2" group-title="VOD | Películas | 1980-1989 | Terror",The Horror of it all (1983)
+https://archive.org/download/thehorrorofitallreel2/thehorrorofitallreel2.mp4
+
+#EXTINF:-1 type="movie" tvg-id="the.-fog.-1980.720p.-blu-ray.x-264.-yify" tvg-name="The Fog 1980-John Carpenter" tvg-logo="https://archive.org/services/img/the.-fog.-1980.720p.-blu-ray.x-264.-yify" group-title="VOD | Películas | 1980-1989 | Terror",The Fog 1980-John Carpenter (1980)
+https://archive.org/download/the.-fog.-1980.720p.-blu-ray.x-264.-yify/The%20Fog%20%281980%29/The.Fog.1980.720p.BluRay.x264.YIFY.mp4
+
+#EXTINF:-1 type="movie" tvg-id="robot-carnival-1987-2160p-pvca" tvg-name="Robot Carnival" tvg-logo="https://archive.org/download/robot-carnival-1987-2160p-pvca/item-image.jpg" group-title="VOD | Películas | 1980-1989 | Cine / General",Robot Carnival (1987)
+https://archive.org/download/robot-carnival-1987-2160p-pvca/robot-carnival-1987-2160p-pvca.mkv
+
+#EXTINF:-1 type="movie" tvg-id="the-agency-1980" tvg-name="The Agency" tvg-logo="https://archive.org/services/img/the-agency-1980" group-title="VOD | Películas | 1980-1989 | Cine / General",The Agency (1980)
+https://archive.org/download/the-agency-1980/The%20Agency%20%281980%29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="unknownchaplin3hiddentreasurereel2" tvg-name="Unknown Chaplin: 3 -- Hidden Treasure" tvg-logo="https://archive.org/services/img/unknownchaplin3hiddentreasurereel2" group-title="VOD | Películas | 1980-1989 | Cine / General",Unknown Chaplin: 3 -- Hidden Treasure (1983)
+https://archive.org/download/unknownchaplin3hiddentreasurereel2/unkownchaplin3hiddentreasurereel2.mp4
+
+#EXTINF:-1 type="movie" tvg-id="ALCFJimHubbardFilmsGayPride1983Clip4628-29" tvg-name="ALCF Jim Hubbard Films Gay Pride 1983 Clip 4628-29" tvg-logo="https://archive.org/services/img/ALCFJimHubbardFilmsGayPride1983Clip4628-29" group-title="VOD | Películas | 1980-1989 | Cine / General",ALCF Jim Hubbard Films Gay Pride 1983 Clip 4628-29 (1983)
+https://archive.org/download/ALCFJimHubbardFilmsGayPride1983Clip4628-29/ALCFJimHubbardFilmsGayPride1983Clip4628-29.mp4
+
+#EXTINF:-1 type="movie" tvg-id="venido-a-menos-1981" tvg-name="Venido A Menos" tvg-logo="https://archive.org/services/img/venido-a-menos-1981" group-title="VOD | Películas | 1980-1989 | Cine / General",Venido A Menos (1981)
+https://archive.org/download/venido-a-menos-1981/Venido%20a%20menos%20%281981%29.mkv
+
+#EXTINF:-1 type="movie" tvg-id="videoplayback_20180203" tvg-name="Mama Dracula" tvg-logo="https://archive.org/services/img/videoplayback_20180203" group-title="VOD | Películas | 1980-1989 | Cine / General",Mama Dracula (1980)
+https://archive.org/download/videoplayback_20180203/videoplayback.mp4
+
